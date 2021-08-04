5c51d2324c75024c3704abcdefab29cf588135b8
tyomakarimov <karartvas170202@gmail.com>
2021-08-04 15:08:18 +0300

    added redirect and tasks page

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,20 +1,34 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Navbar from '../components/Navbar/Navbar';
 import Login from './Authentication/Login';
 import Register from './Authentication/Register';
+import Tasks from './Tasks/Tasks';
+import { RootState } from '../store';
 
 const App: React.FC = () => {
+  const isAuthenticated = useSelector((state: RootState) => state.auth.authenticated);
   return (
     <>
       <Navbar />
       <main>
         <Switch>
+          <Route path="/" exact>
+            {!isAuthenticated && <Redirect to='/login' />}
+            {isAuthenticated && <Redirect to='/tasks' />}
+          </Route>
           <Route path="/login">
-            <Login />
+            {!isAuthenticated && <Login />}
+            {isAuthenticated && <Redirect to='/tasks' />}
           </Route>
           <Route path="/register">
-            <Register />
+            {!isAuthenticated && <Register />}
+            {isAuthenticated && <Redirect to='/tasks' />}
+          </Route>
+          <Route path="/tasks">
+            {isAuthenticated && <Tasks />}
+            {!isAuthenticated && <Redirect to='/login' />}
           </Route>
         </Switch>
       </main>
---
+++ frontend/src/pages/Tasks/Tasks.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import classes from './Tasks.module.scss';
+
+const Tasks: FC = () => {
+  return <h1 className={classes.tasks}>Your tasks</h1>;
+};
+
+export default Tasks;
